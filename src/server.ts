1036f7f6f143dd4c598d6c0401dcacf5677a0420
Anthony Forsythe <forsythetony@gmail.com>
2020-06-08 23:17:50 -0500

    Allowing CORS

--- src/server.ts
+++ src/server.ts
@@ -18,15 +18,12 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
 
   app.use(bodyParser.json());
 
-  // app.use(cors({
-  //   allowedHeaders: [
-  //     'Origin', 'X-Requested-With',
-  //     'Content-Type', 'Accept',
-  //     'X-Access-Token', 'Authorization',
-  //   ],
-  //   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  //   origin: 'localhost:8080',
-  // }));
+  //CORS Should be restricted
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 
   app.use('/api/v0/', IndexRouter);
 
