dcd9d2357bbd146595701cbebaff1be85844324c
Anthony Forsythe <forsythetony@gmail.com>
2020-06-16 12:59:58 -0500

    Updating CORS with frontend URL loaded from env variable

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-// import cors from 'cors';
+import cors from 'cors';
 import express from 'express';
 import {sequelize} from './sequelize';
 
@@ -18,12 +18,15 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
 
   app.use(bodyParser.json());
 
-  //CORS Should be restricted
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
+  app.use(cors({
+    allowedHeaders: [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization',
+    ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: process.env.UDAGRAM_FRONTEND_URL,
+  }));
 
   app.use('/api/v0/', IndexRouter);
 
